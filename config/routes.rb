d8f69471c040b9fc6afc6e6b051676ce4facdb9d
EC2 Default User <104186291+t-yuusuke@users.noreply.github.com>
2022-05-16 17:19:37 +0000

    [Finish]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
+devise_for :users
+
+ # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :postlmages, only: [:new, :index, :show]
 
 get 'homes/about' => 'homes#about', as: 'about'
 
