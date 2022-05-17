b9dd1e75027d9c079dd9bcdb06a53ebe525fb31f
EC2 Default User <104186291+t-yuusuke@users.noreply.github.com>
2022-05-17 11:05:05 +0000

    [Finish]4-14

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ devise_for :users
 
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources :postlmages, only: [:new, :create, :index, :show, :destroy]
+resources :users, only: [:show, :edit]
 
 get 'homes/about' => 'homes#about', as: 'about'
 
