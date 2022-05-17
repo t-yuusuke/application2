17e40a01338265c9f74486b0b94ae078dfecc372
EC2 Default User <104186291+t-yuusuke@users.noreply.github.com>
2022-05-17 10:27:32 +0000

    [Finish]4-13

--- app/controllers/postlmages_controller.rb
+++ app/controllers/postlmages_controller.rb
@@ -1,15 +1,15 @@
 class PostlmagesController < ApplicationController
-  
+
   def new
     @postlmage = Postlmage.new
   end
-  
+
   def create
     @postlmage = Postlmage.new(postlmage_params)
     @postlmage.user_id = current_user.id
     @postlmage.save
     redirect_to postlmages_path
-  end  
+  end
 
   def index
     @postlmages = Postlmage.all
@@ -18,12 +18,15 @@ class PostlmagesController < ApplicationController
   def show
     @postlmage = Postlmage.find(params[:id])
   end
-  
+
   def destroy
+    @postlmage = Postlmage.find(params[:id])
+    @postlmage.destroy
+    redirect_to '/postlmages'
   end
-  
+
   private
-  
+
   def postlmage_params
   params.require(:postlmage).permit(:shop_name, :image, :caption)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ root to: "homes#top"
 devise_for :users
 
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :postlmages, only: [:new, :create, :index, :show]
+resources :postlmages, only: [:new, :create, :index, :show, :destroy]
 
 get 'homes/about' => 'homes#about', as: 'about'
 
